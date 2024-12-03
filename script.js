eefb4dc041bac2d84186c993a767c4935de49a08
jwoodsjr <joseph.e.woods.jr@gmail.com>
2024-12-03 13:09:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function showit(var1){
+function showit (var1) {
 // Hide the one div that is shown
 document.getElementById('d1').style.display='none';
 document.getElementById('d2').style.display='none';
 document.getElementById('d3').style.display='none';
 document.getElementById('d4').style.display='none';
+document.getElementById('d5').style.display='none';
 // show the one div that you sent to this function
 document.getElementById(var1).style.display='block';
 }
